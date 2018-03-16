034586e1aa152fdb7c45d321d097191948e93559
mario <mario.esquire@gmail.com>
2018-03-17 00:54:26 +0100

    a

---
+++ pt_lab_1/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pt_lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Backend.LoadDirectoryView();
+            ClearTreeView();
+
+            foreach (string s in Directory.EnumerateDirectories(path))
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s.Substring(s.LastIndexOf('\\') + 1);
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+
+                foreach (string f in Directory.GetFiles(s))
+                {
+                    TreeViewItem tviFile = new TreeViewItem();
+                    tviFile.Header = System.IO.Path.GetFileName(f);
+                    tviFile.Tag = f;
+                    tviFile.FontWeight = FontWeights.Normal;
+                    item.Items.Add(tviFile);
+                }
+
+                FillTreeView(item, s);
+
+                tvFiles.Items.Add(item);
+            }
+            //FillTreeView(item, path, firstRun: true);
+        }
+
+        private void FillTreeView(TreeViewItem parentItem, string path)
+        {
+            foreach (string str in Directory.EnumerateDirectories(path))
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = str.Substring(str.LastIndexOf('\\') + 1);
+                item.Tag = str;
+                item.FontWeight = FontWeights.Normal;
+                foreach (string f in Directory.GetFiles(str))
+                {
+                    TreeViewItem tviFile = new TreeViewItem();
+                    tviFile.Header = System.IO.Path.GetFileName(f);
+                    tviFile.Tag = f;
+                    tviFile.FontWeight = FontWeights.Normal;
+                    item.Items.Add(tviFile);
+                }
+                parentItem.Items.Add(item);
+
+                FillTreeView(item, str);
+            }
+        }
+
+        private void ClearTreeView()
+        {
+            this.tvFiles.Items.Clear();
+        }
+
+        private void ctxMenu_ShowTxt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string path = ((System.Windows.Controls.TreeViewItem)tvFiles.SelectedValue).Tag.ToString();
+                if (!File.GetAttributes(path).HasFlag(FileAttributes.Directory)
+                    && System.IO.Path.GetExtension(path) == ".txt")
+                {
+                    string fileContents;
+                    using (var textReader = File.OpenText(path))
+                    {
+                        fileContents = textReader.ReadToEnd();
+                    }
+                    txtBox_Main.Text = fileContents;
+                }
+            }
+            catch
+            {
+                //implement
+            }
+        }
+
+        private void ctxMenu_DeleteFromTree(object sender, RoutedEventArgs e)
+        {
+            string path = ((System.Windows.Controls.TreeViewItem)tvFiles.SelectedValue).Tag.ToString();
+            DialogResult result = System.Windows.Forms.MessageBox.Show(
+                "Are you sure you want to delete the resource?","Delete resource?",System.Windows.Forms.MessageBoxButtons.YesNo);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+                {
+                    Directory.Delete(path);
+                }
+                else
+                {
+                    File.Delete(path);
+                }
+            }
+            tvFiles.Items.Remove(tvFiles.SelectedItem);
+            tvFiles.UpdateLayout();
+        }
+    }
+}
+
---
+++ pt_lab_1/backend.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pt_lab_1
+{
+    class Backend
+    {
+
+        internal static string LoadDirectoryView()
+        {
+            var dlg = new FolderBrowserDialog() { Description = "Select directory to open" };
+            dlg.ShowDialog();
+            return dlg.SelectedPath;
+        }
+
+
+        internal static void AppendDirectoriesToTreeNode(TreeNode node, string root)
+        {
+            DirectoryInfo rootDir = new DirectoryInfo(root);
+
+            foreach (DirectoryInfo subDir in rootDir.GetDirectories())
+            {
+                TreeNode subdirNode = new TreeNode(subDir.Name);
+                AppendDirectoriesToTreeNode(subdirNode, subDir.FullName);
+
+                foreach (FileInfo fileInfo in subDir.GetFiles())
+                {
+                    subdirNode.Nodes.Add(fileInfo.Name);
+                }
+
+                node.Nodes.Add(subdirNode);
+            }
+        }
+
+    }
+}
